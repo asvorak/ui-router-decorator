3587f339413172e632936703dbd756e06006a50a
Andrii.Svorak <andrew.svorak@gmail.com>
2018-06-19 23:15:30 +0300

    Some updates.

--- state.interface.ts
+++ state.interface.ts
@@ -1,3 +1,5 @@
+import {IState as IStateAngular} from '@types/angular-ui-router';
+
 /**
  * A controller class with the state config applied as a property
  */
@@ -8,6 +10,6 @@ export interface IStateController {
 /**
  * Angular UI IState interface with the name parameter being required
  */
-export interface IState extends angular.ui.IState {
+export interface IState extends IStateAngular {
     name: string;
 }
